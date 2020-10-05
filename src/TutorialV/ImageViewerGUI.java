bd6b2e9cee881083c191c044cfe780142f23d948
thebigsasha <sashathethird@gmail.com>
2020-10-05 12:01:36 -0400

    Addsort

--- src/TutorialV/ImageViewerGUI.java
+++ src/TutorialV/ImageViewerGUI.java
@@ -59,7 +59,16 @@ public class ImageViewerGUI {
     //TODO: Write here!
     private void bubbleSort(Color arr[])
     {
-  
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++)
+            for (int j = 0; j < n-i-1; j++)
+                if (arr[j].getRGB() > arr[j+1].getRGB())
+                {
+                    Color temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+
+                }
     }
     //========================================
 
